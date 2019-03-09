d2cee1faf36572f7d33f17d064865178029baf4f
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-03-09 18:45:22 +0100

    Make t_ID work

--- scanner.py
+++ scanner.py
@@ -3,22 +3,29 @@
 import sys
 import ply.lex as lex
 
-tokens = (
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
-    'LPAREN',
-    'RPAREN',
-    'NUMBER',
-)
+literals = "+-*/=()[]{}:',;<>"
 
-t_PLUS   = r'\+'
-t_MINUS  = r'-'
-t_TIMES  = r'\*'
-t_DIVIDE = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
+tokens = ('ELEMWISE_PLUS', 'ELEMWISE_MINUS', 'ELEMWISE_MUL', 'ELEMWISE_DIV',
+          'ASSIGN_PLUS', 'ASSIGN_MINUS', 'ASSIGN_MUL', 'ASSIGN_DIV', 'LEQ',
+          'GEQ', 'NEQ', 'EQ', 'NUMBER', 'FLOAT', 'ID', 
+          )
+
+t_ELEMWISE_PLUS = r'\.+'
+t_ELEMWISE_MINUS = r'\.+'
+t_ELEMWISE_MUL = 'r\.\*'
+t_ELEMWISE_DIV = 'r\./'
+
+t_ASSIGN_PLUS = r'\+='
+t_ASSIGN_MINUS = r'-='
+t_ASSIGN_MUL = r'\*='
+t_ASSIGN_DIV = r'/='
+
+t_LEQ = r'<='
+t_GEQ = r'>='
+t_NEQ = r'!='
+t_EQ = r'=='
+
+#t_STRING = r'.+'
 
 t_ignore = ' \t'
 
@@ -27,10 +34,10 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
-
 
 def t_error(t):
     print("line %d: illegal character '%s'" %(t.lineno, t.value[0]) )
@@ -39,6 +46,11 @@ def t_error(t):
 def t_comment(t):
     r'\#.*'
     pass
+
+
+def t_ID(t):
+    r'[a-zA-Z_]\w*'
+    return t
 
 lexer = lex.lex()
 fh = None
