c1c1eaecacc7e2aafa568b0ecd3762bd3e205e4e
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-03-09 23:15:03 +0100

    Fix t_FLOAT to reject 123e

--- scanner.py
+++ scanner.py
@@ -33,7 +33,7 @@ t_ignore = ' \t'
 
 def t_FLOATNUM(t):
     # fraction part can be omitted if exponent is present
-    r'(?i)[+-]?\d*((\d\.|\.\d)\d*|\B(?=e))(e[+-]?\d+)?'
+    r'(?i)[+-]?\d*((\d\.|\.\d)\d*(e[+-]?\d+)?|\d(e[+-]?\d+))'
     t.value = float(t.value)
     return t
 
