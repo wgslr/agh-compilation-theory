5671852bd9b72b71d540002cad61c0a164fda792
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-03-09 22:06:39 +0100

    Handle keywords, track column number

--- scanner.py
+++ scanner.py
@@ -4,17 +4,21 @@ import sys
 import ply.lex as lex
 literals = "+-*/=()[]{}:',;<>"
 
-tokens = ('DOTSUM', 'DOTSUB', 'DOTMUL', 'DOTDIV',
-          'SUMASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN', 'LEQ',
-          'GEQ', 'NEQ', 'EQ', 'INTNUM', 'FLOATNUM', 'ID', 
-          )
 
-t_DOTSUM = r'\.\+'
+keywords = ('IF', 'ELSE', 'FOR', 'WHILE', 'BREAK', 'CONTINUE',
+            'RETURN', 'EYE', 'ZEROS', 'ONES', 'PRINT')
+
+tokens = ('DOTADD', 'DOTSUB', 'DOTMUL', 'DOTDIV',
+          'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN', 'LEQ',
+          'GEQ', 'NEQ', 'EQ', 'INTNUM', 'FLOATNUM', 'ID',
+          ) + keywords
+
+t_DOTADD = r'\.\+'
 t_DOTSUB = r'\.-'
 t_DOTMUL = r'\.\*'
 t_DOTDIV = r'\./'
 
-t_SUMASSIGN = r'\+='
+t_ADDASSIGN = r'\+='
 t_SUBASSIGN = r'-='
 t_MULASSIGN = r'\*='
 t_DIVASSIGN = r'/='
@@ -28,24 +32,29 @@ t_EQ = r'=='
 
 t_ignore = ' \t'
 
+
 def t_FLOATNUM(t):
     # fraction part can be omitted if exponent is present
     r'(?i)[+-]?\d*((\d\.|\.\d)\d*|\B(?=e))(e[+-]?\d+)?'
     t.value = float(t.value)
     return t
 
+
 def t_INTNUM(t):
     r'[+-]?\d+'
     t.value = int(t.value)
     return t
 
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+
 def t_error(t):
-    print("line %d: illegal character '%s'" %(t.lineno, t.value[0]) )
+    print("line %d: illegal character '%s'" % (t.lineno, t.value[0]))
     t.lexer.skip(1)
+
 
 def t_comment(t):
     r'\#.*'
@@ -53,8 +62,18 @@ def t_comment(t):
 
 
 def t_ID(t):
-    r'[a-zA-Z_]\w*'
+    r'[a-zA-mZ_]\w*'
+
+    if t.value.upper() in keywords:
+        t.type = t.value.upper()
+
     return t
+
+
+def find_column(input, token):
+    line_start = input.rfind('\n', 0, token.lexpos) + 1
+    return (token.lexpos - line_start) + 1
+
 
 lexer = lex.lex()
 fh = None
