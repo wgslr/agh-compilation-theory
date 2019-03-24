0c6a076977c2f3ed1e74246f1b827457d369e929
Michał Stanisz <mch.stanisz@gmail.com>
2019-03-24 15:32:11 +0100

    Add binary operation parsing and assignments

--- 02-parser/Mparser.py
+++ 02-parser/Mparser.py
@@ -3,45 +3,149 @@
 import scanner
 import ply.yacc as yacc
 
-
 tokens = scanner.tokens
 literals = scanner.literals
 
+symtab = {}
+
 precedence = (
-   # to fill ...
-   ("left", '+', '-'),
-   # to fill ...
+    ("right", '=', 'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN'),
+    ("left", '+', '-'),
+    ("left", 'DOTADD', 'DOTSUB'),
+    ("left", '*', '/'),
+    ("left", 'DOTMUL', 'DOTDIV'),
+    ('right', 'UMINUS'),
 )
 
 
 def p_error(p):
     if p:
-        print("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')".format(p.lineno, scanner.find_tok_column(p), p.type, p.value))
+        print("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')".format(p.lineno,
+                                                                                  scanner.find_column(p.lexer.lexdata,
+                                                                                                      p),
+                                                                                  p.type, p.value))
     else:
         print("Unexpected end of input")
 
 
-def p_program(p):
-    """program : instructions_opt"""
+def p_start(p):
+    """start : expression
+             | start expression"""
+    # if len(p) == 2:
+    #     print("p[1]=", p[1])
+    # else:
+    #     print("p[2]=", p[2])
+    p[0] = p[1]
 
-def p_instructions_opt_1(p):
-    """instructions_opt : instructions """
 
-def p_instructions_opt_2(p):
-    """instructions_opt : """
+def p_expr(p):
+    """expression : assignment
+                  | assignment ';'
+                  | operation
+                  | operation ';'"""
+    p[0] = p[1]
 
-def p_instructions_1(p):
-    """instructions : instructions instruction """
 
-def p_instructions_2(p):
-    """instructions : instruction """
+def p_assignment(p):
+    """assignment : ID '=' operation"""
+    symtab[p[1]] = p[3]
+    p[0] = p[3]
+
+
+def p_addassignment(p):
+    """assignment : ID ADDASSIGN operation"""
+    symtab[p[1]] += p[3]
+    p[0] = symtab[p[1]]
+
+
+def p_subassignment(p):
+    """assignment : ID SUBASSIGN operation"""
+    symtab[p[1]] -= p[3]
+    p[0] = symtab[p[1]]
+
+
+def p_mulassignment(p):
+    """assignment : ID MULASSIGN operation"""
+    symtab[p[1]] *= p[3]
+    p[0] = symtab[p[1]]
+
+
+def p_divassignment(p):
+    """assignment : ID DIVASSIGN operation"""
+    symtab[p[1]] /= p[3]
+    p[0] = symtab[p[1]]
+
+
+def p_var(p):
+    """var : ID"""
+    val = symtab.get(p[1])
+    if val:
+        p[0] = val
+    else:
+        p[0] = 1
+
+
+def p_num(p):
+    """num : INTNUM 
+           | FLOATNUM
+           | var"""
+    p[0] = p[1]
+
+
+def p_neg(p):
+    """neg_num : '-' num %prec UMINUS"""
+    p[0] = -p[2]
+
+
+def p_operation(p):
+    """operation : num
+                 | neg_num"""
+    p[0] = p[1]
+
+
+def p_operation_sum(p):
+    """operation : operation '+' operation
+                 | operation '-' operation"""
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    else:
+        p[0] = p[1] - p[3]
+
+
+def p_operation_dot_sum(p):
+    """operation : operation DOTADD operation
+                 | operation DOTSUB operation"""
+    if p[2] == '.+':
+        p[0] = p[1] + p[3]
+    else:
+        p[0] = p[1] - p[3]
+
+
+def p_operation_mul(p):
+    """operation : operation '*' operation
+                 | operation '/' operation"""
+    if p[2] == '*':
+        p[0] = p[1] * p[3]
+    else:
+        p[0] = p[1] / p[3]
+
+
+def p_operation_dot_mul(p):
+    """operation : operation DOTMUL operation
+                 | operation DOTDIV operation"""
+    if p[2] == '.*':
+        p[0] = p[1] * p[3]
+    else:
+        p[0] = p[1] / p[3]
+
+
+def p_operation_group(p):
+    """operation : '(' operation ')'"""
+    p[0] = p[2]
+
 
 # to finish the grammar
 # ....
 
 
-    
-
-
 parser = yacc.yacc()
-
