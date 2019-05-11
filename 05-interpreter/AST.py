8c4a075ac408fb0de6829bf2c98f5922909afb70
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-05-11 17:41:02 +0200

    05: Reuse files from previous task

---
+++ 05-interpreter/AST.py
@@ -0,0 +1,142 @@
+
+
+class Node(object):
+    pass
+
+
+class Instructions(Node):
+    def __init__(self, lineno, nodes):
+        self.lineno = lineno
+        self.nodes = nodes
+
+
+class FlowKeyword(Node):
+    def __init__(self, lineno, keyword):
+        self.lineno = lineno
+        self.keyword = keyword.upper()
+
+
+class Print(Node):
+    def __init__(self, lineno, arguments):
+        self.lineno = lineno
+        self.arguments = arguments
+
+
+class Return(Node):
+    def __init__(self, lineno, value=None):
+        self.lineno = lineno
+        """argument may be None for return statement
+        without a return value"""
+        self.value = value
+
+
+class String(Node):
+    def __init__(self, lineno, value):
+        self.lineno = lineno
+        self.value = value
+
+
+class Vector(Node):
+    def __init__(self, lineno, elements):
+        self.lineno = lineno
+        self.elements = elements
+
+
+class Matrix(Vector):
+    pass
+
+
+class Reference(Node):
+    """A matrix cell reference"""
+
+    def __init__(self, lineno, name, coords):
+        self.lineno = lineno
+        self.name = name
+        self.coords = coords
+
+
+class FunctionCall(Node):
+    def __init__(self, lineno, func_name, arguments):
+        self.lineno = lineno
+        self.name = func_name
+        self.arguments = arguments
+
+
+class While(Node):
+    def __init__(self, lineno, condition, body):
+        self.lineno = lineno
+        self.condition = condition
+        self.body = body
+
+
+class For(Node):
+    def __init__(self, lineno, iterator, start, end, body):
+        self.lineno = lineno
+        self.iterator = iterator
+        self.range = Range(lineno, start, end)
+        self.body = body
+
+
+class Range(Node):
+    def __init__(self, lineno, start, end):
+        self.lineno = lineno
+        self.start = start
+        self.end = end
+
+
+class Variable(Node):
+    def __init__(self, lineno, name):
+        self.lineno = lineno
+        self.name = name
+
+
+class If(Node):
+    def __init__(self, lineno, condition, body, else_body=None):
+        self.lineno = lineno
+        self.condition = condition
+        self.body = body
+        self.else_body = else_body
+
+
+class BinExpr(Node):
+    def __init__(self, lineno, op, left, right):
+        self.lineno = lineno
+        self.op = op
+        self.left = left
+        self.right = right
+
+
+class ArithmeticOperation(BinExpr):
+    pass
+
+
+class Assignment(BinExpr):
+    pass
+
+
+class IntNum(Node):
+    def __init__(self, lineno, value):
+        self.lineno = lineno
+        self.value = value
+
+
+class FloatNum(Node):
+    def __init__(self, lineno, value):
+        self.lineno = lineno
+        self.value = value
+
+
+class UnaryExpr(Node):
+    def __init__(self, lineno, operation, operand):
+        self.lineno = lineno
+        self.operation = operation
+        self.operand = operand
+
+
+class Comparison(BinExpr):
+    pass
+
+
+class Error(Node):
+    def __init__(self):
+        pass
---
+++ 05-interpreter/Mparser.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+import scanner
+import ply.yacc as yacc
+
+import AST
+
+tokens = scanner.tokens
+literals = scanner.literals
+
+
+precedence = (
+    ('nonassoc', 'IF'),
+    ('nonassoc', 'ELSE'),
+    ('right', '=', 'ADDASSIGN', 'SUBASSIGN', 'MULASSIGN', 'DIVASSIGN'),
+    ('nonassoc', '<', '>', 'EQ', 'NEQ', 'GEQ', 'LEQ'),
+    ('left', '+', '-'),
+    ('left', 'DOTADD', 'DOTSUB'),
+    ('left', '*', '/'),
+    ('left', 'DOTMUL', 'DOTDIV'),
+    ('right', 'UMINUS'),
+    ('right', '\''),
+)
+
+
+def p_error(p):
+    if p:
+        # using lexer object for convenient global state store
+        p.lexer.encountered_error = True
+        print("Syntax error at line {0}, column {1}: LexToken({2}, '{3}')"
+              .format(p.lineno, scanner.find_column(p.lexer.lexdata, p),
+                      p.type, p.value))
+    else:
+        print("Unexpected end of input")
+
+
+# -------------------------
+# Main productions
+# -------------------------
+
+def p_instructions(p):
+    """instructions : instruction
+                    | instruction instructions"""
+    if len(p) == 2:
+        p[0] = AST.Instructions(p.lineno(1), [p[1]])
+    else:
+        p[0] = p[2]
+        p[0].nodes = [p[1]] + p[0].nodes
+
+
+def p_instruction(p):
+    """instruction : block
+                   | conditional
+                   | loop
+                   | statement ';'
+                   | error ';'"""
+    p[0] = p[1]
+
+
+def p_statement(p):
+    """statement : assignment
+                 | flow_keyword
+                 | return
+                 | print"""
+    p[0] = p[1]
+
+
+def p_flow_keyword(p):
+    """flow_keyword : BREAK
+                    | CONTINUE"""
+    p[0] = AST.FlowKeyword(p.lineno(1), p[1])
+
+
+def p_expression(p):
+    """expression : comparison_expression
+                  | numeric_expression"""
+    p[0] = p[1]
+
+
+def p_block(p):
+    """block : '{' instructions '}'
+             | '{' error '}'"""
+    p[0] = p[2]
+
+
+def p_print(p):
+    """print : PRINT print_body"""
+    p[0] = AST.Print(p.lineno(1), p[2])
+
+
+def p_print_body(p):
+    """print_body : string
+                  | expression
+                  | string ',' print_body
+                  | expression ',' print_body"""
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[3]
+        p[0] = [p[1]] + p[0]
+
+
+def p_return(p):
+    """return : RETURN expression
+              | RETURN"""
+    if len(p) == 2:
+        p[0] = AST.Return(p.lineno(1))
+    else:
+        p[0] = AST.Return(p.lineno(1), p[2])
+
+
+def p_string(p):
+    """string : STRING"""
+    p[0] = AST.String(p.lineno(1), p[1])
+
+# -------------------------
+# Matrices
+# -------------------------
+
+
+def p_vector(p):
+    """vector : '[' vector_body ']'
+              | '[' ']'"""
+    if len(p) == 3:
+        p[0] = AST.Vector(p.lineno(1), [])
+    else:
+        p[0] = AST.Vector(p.lineno(1), p[2])
+
+
+def p_vector_body(p):
+    """vector_body : numeric_expression
+                   | vector_body ',' numeric_expression"""
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1]
+        p[0] += [p[3]]
+
+
+def p_matrix(p):
+    """matrix : '[' matrix_body ']'
+              | '[' ']'"""
+    if len(p) == 3:
+        p[0] = AST.Matrix(p.lineno(1), [])
+    else:
+        p[0] = AST.Matrix(p.lineno(1), p[2])
+
+
+def p_matrix_body(p):
+    """matrix_body : vector
+                   | matrix_body ',' vector"""
+    if len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = p[1]
+        p[0] += [p[3]]
+
+
+# -------------------------
+# Arrays
+# -------------------------
+
+def p_array_range(p):
+    """array_range : var '[' expression ',' expression ']'"""
+    p[0] = AST.Reference(p.lineno(1), p[1], [p[3], p[5]])
+
+
+# -------------------------
+# Funs
+# -------------------------
+
+def p_fun(p):
+    """fun : fun_name '(' vector_body ')'
+           | fun_name '(' error ')'"""
+    p[0] = AST.FunctionCall(p.lineno(1), p[1], p[3])
+
+
+def p_fun_name(p):
+    """fun_name : ZEROS
+                | ONES
+                | EYE"""
+    p[0] = p[1]
+
+
+# -------------------------
+# Loops
+# -------------------------
+
+def p_loop(p):
+    """loop : while
+            | for"""
+    p[0] = p[1]
+
+
+def p_while(p):
+    """while : WHILE '(' expression ')' instruction"""
+    p[0] = AST.While(p.lineno(1), p[3], p[5])
+
+
+def p_for(p):
+    """for : FOR ID '=' numeric_expression ':' numeric_expression instruction"""
+    p[0] = AST.For(p.lineno(1), AST.Variable(p.lineno(1), p[2]), p[4], p[6], p[7])
+
+
+# -------------------------
+# If statements
+# -------------------------
+
+def p_conditional(p):
+    """conditional : IF '(' expression ')' instruction %prec IF
+                   | IF '(' expression ')' instruction ELSE instruction"""
+    if len(p) == 6:
+        else_body = None
+    else:
+        else_body = p[7]
+    p[0] = AST.If(p.lineno(1), p[3], p[5], else_body)
+
+
+# -------------------------
+# Assignments
+# -------------------------
+
+def p_assignment_lhs(p):
+    """assignment_lhs : var
+                      | array_range"""
+    p[0] = p[1]
+
+
+def p_assignment(p):
+    """assignment : assignment_lhs assignment_operator expression
+                  | assignment_lhs '=' string"""
+    p[0] = AST.Assignment(p.lineno(1), p[2], p[1], p[3])
+
+
+def p_assignment_operator(p):
+    """assignment_operator : '='
+                           | ADDASSIGN
+                           | SUBASSIGN
+                           | MULASSIGN
+                           | DIVASSIGN"""
+    p[0] = p[1]
+
+
+# -------------------------
+# Numerics and variables
+# -------------------------
+
+def p_var(p):
+    """var : ID
+           | var '[' vector_body ']'"""
+    if len(p) == 2:
+        p[0] = AST.Variable(p.lineno(1), p[1])
+    else:
+        p[0] = AST.Reference(p.lineno(1), p[1], p[3])
+
+
+def p_num(p):
+    """num : INTNUM
+           | FLOATNUM
+           | var"""
+    if isinstance(p[1], (int, long)):
+        p[0] = AST.IntNum(p.lineno(1), p[1])
+    elif isinstance(p[1], float):
+        p[0] = AST.FloatNum(p.lineno(1), p[1])
+    else:
+        p[0] = p[1]
+
+
+# -------------------------
+# Numeric expressions
+# -------------------------
+
+def p_unary_op(p):
+    """unary_op : negation
+                | transposition"""
+    p[0] = p[1]
+
+
+def p_neg(p):
+    """negation : '-' numeric_expression %prec UMINUS"""
+    p[0] = AST.UnaryExpr(p.lineno(1), 'NEGATE', p[2])
+
+
+def p_transposition(p):
+    r"""transposition : numeric_expression '\''"""
+    p[0] = AST.UnaryExpr(p.lineno(1), 'TRANSPOSE', p[1])
+
+
+def p_numeric_expression(p):
+    """numeric_expression : num
+                          | matrix
+                          | vector
+                          | unary_op
+                          | fun
+                          | '(' numeric_expression ')'"""
+    if p[1] == '(':
+        p[0] = p[2]
+    else:
+        p[0] = p[1]
+
+
+def p_bin_numeric_expression(p):
+    """numeric_expression : numeric_expression '+' numeric_expression
+                          | numeric_expression '-' numeric_expression
+                          | numeric_expression '*' numeric_expression
+                          | numeric_expression '/' numeric_expression
+                          | numeric_expression DOTADD numeric_expression
+                          | numeric_expression DOTSUB numeric_expression
+                          | numeric_expression DOTMUL numeric_expression
+                          | numeric_expression DOTDIV numeric_expression"""
+    p[0] = AST.ArithmeticOperation(p.lineno(1), p[2], p[1], p[3])
+
+
+# -------------------------
+# Binary comparisons expressions
+# -------------------------
+
+def p_comparison_expression(p):
+    """comparison_expression : numeric_expression '<' numeric_expression
+           | numeric_expression '>' numeric_expression
+           | numeric_expression EQ numeric_expression
+           | numeric_expression NEQ numeric_expression
+           | numeric_expression GEQ numeric_expression
+           | numeric_expression LEQ numeric_expression
+           | '(' comparison_expression ')'"""
+    if p[1] == '(':
+        p[0] = p[2]
+    else:
+        p[0] = AST.Comparison(p.lineno(1), p[2], p[1], p[3])
+
+
+parser = yacc.yacc()
